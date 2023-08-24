da0d7bbf5ab0b6cd2d937304271a5cf5c83f0da4
Almaz Khannanov <yourrussianweapon@gmail.com>
2023-08-24 12:49:49 +0500

    fix(updated links on all *.html's + db fix)

--- src/main/resources/db/changelog/DDL/002_create_custom_replies_table.sql
+++ src/main/resources/db/changelog/DDL/002_create_custom_replies_table.sql
@@ -2,7 +2,7 @@
 --changeset myname:create-multiple-tables splitStatements:true endDelimiter:;
 
 CREATE TABLE `custom_bot_replies`
-(`default_text` TINYINT CHECK (`default_text` BETWEEN 0 AND 30),
+(`default_text` BIGINT,
 `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 `custom_text` VARCHAR(255));
 
